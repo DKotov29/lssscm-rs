229c3d2ca5de720b235c11f541e17092f3101881
DKotov <dkotov292@gmail.com>
2023-01-12 23:19:42 +0200

    it works! but not all...

--- lsscm/src/lib.rs
+++ lsscm/src/lib.rs
@@ -1,12 +1,13 @@
 extern crate core;
 
-use std::cmp::max;
 use crate::NumberError::{Parse, UnImplemented};
 use itertools::{EitherOrBoth, Itertools};
-use std::fmt::{Binary, Debug, Display, Formatter, LowerHex};
+use std::cmp::max;
+use std::fmt::{Binary, Debug, Display, format, Formatter, LowerHex};
 use std::num::ParseIntError;
-use std::ops::{Add, Shl, Shr, Sub};
+use std::ops::{Add, Mul, Shl, Shr, Sub};
 
+#[derive(Debug)]
 pub struct Number {
     v: Vec<u64>,
 }
@@ -17,25 +18,29 @@ pub enum NumberError {
     Parse,
 }
 
+#[derive(Debug)]
 pub struct OverflowSub;
 
 impl LowerHex for Number {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut s = String::new();
         for i in self.v.iter().rev() {
-            write!(f, "{:016x}", i)?;
+            s.push_str(format!("{:0>16x}", i).as_str());
         }
+        write!(f, "{}", s.trim_start_matches("0"))?;
+
         Ok(())
     }
 }
 
-impl Binary for Number {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for i in self.v.iter().rev() {
-            write!(f, "{:064b}", i)?;
-        }
-        Ok(())
-    }
-}
+// impl Binary for Number {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//         for i in self.v.iter() {
+//             write!(f, "{:064b}", i.reverse_bits())?;
+//         }
+//         Ok(())
+//     }
+// }
 
 impl Add<&Number> for &Number {
     type Output = Number;
@@ -43,16 +48,16 @@ impl Add<&Number> for &Number {
     fn add(self, rhs: &Number) -> Self::Output {
         let mut v: Vec<u64> = Vec::with_capacity(max(self.v.len(), rhs.v.len()));
         let mut carry: bool = false;
-        for i in self.v.iter().rev().zip_longest(rhs.v.iter().rev()) {
+        for i in self.v.iter().zip_longest(rhs.v.iter()) {
             let mut left: u64 = 0;
             let mut right: u64 = 0;
             match i {
                 EitherOrBoth::Both(x, y) => {
-                    left = *x;
-                    right = *y;
+                    left = (*x);
+                    right = (*y);
                 }
                 EitherOrBoth::Left(x) | EitherOrBoth::Right(x) => {
-                    left = *x;
+                    left = (*x);
                 }
             }
             if carry {
@@ -72,20 +77,19 @@ impl Add<&Number> for &Number {
             }
         }
         if carry {
-            v.push(1);
+            v.push(1u64);
         }
         Number { v }
     }
 }
 
-//todo
 impl Sub<&Number> for &Number {
     type Output = Result<Number, OverflowSub>;
 
     fn sub(self, rhs: &Number) -> Self::Output {
         let mut v: Vec<u64> = Vec::with_capacity(max(self.v.len(), rhs.v.len()));
         let mut carry: bool = false;
-        for i in self.v.iter().rev().zip_longest(rhs.v.iter()) {
+        for i in self.v.iter().zip_longest(rhs.v.iter()) {
             let mut left: u64 = 0;
             let mut right: u64 = 0;
             match i {
@@ -93,9 +97,7 @@ impl Sub<&Number> for &Number {
                     left = *x;
                     right = *y;
                 }
-                EitherOrBoth::Left(x) => {
-                    left = *x
-                }
+                EitherOrBoth::Left(x) => left = *x,
                 EitherOrBoth::Right(x) => {
                     right = *x;
                 }
@@ -116,7 +118,9 @@ impl Sub<&Number> for &Number {
                 carry = car;
             }
         }
-        // if carry { return Err(OverflowSub) }
+        if carry {
+            return Err(OverflowSub);
+        }
         Ok(Number { v })
     }
 }
@@ -127,63 +131,137 @@ impl From<ParseIntError> for NumberError {
     }
 }
 
-// todo not work correct for > 64
-impl Shl<usize> for Number {
-    type Output = Number;
-
-    fn shl(self, rhs: usize) -> Self::Output {
-        let mut vec = Vec::with_capacity(self.v.len());
-        let right_shift = 64 - rhs;
-        let mut other: u64 = 0;
-        for i in self.v {
-            vec.push((i << rhs) | other);
-            other = i >> right_shift;
+impl PartialEq<Self> for Number {
+    fn eq(&self, other: &Self) -> bool {
+        let mut res = true;
+        for x in self.v.iter().zip_longest(other.v.iter()) {
+            match x {
+                EitherOrBoth::Both(a, b) => {
+                    if a != b { res = false; break; }
+                }
+                EitherOrBoth::Left(&a) | EitherOrBoth::Right(&a) =>
+                    {if a == 0u64 { res = false; break; }}
+            }
         }
-        vec.push(other);
-        Number { v: vec }
+        res
     }
 }
 
-// todo not work correct for > 64
-impl Shr<usize> for Number {
+impl Mul for &Number {
     type Output = Number;
 
-    fn shr(self, rhs: usize) -> Self::Output {
-        // if rhs > 64 {
-        //
-        // }
-        let left_shift = 64 - rhs;
-        let mut other: u64 = 0;
-        Number {
-            v: self.v.iter().rev().map(|f| {
-                let m = (f >> rhs) | other;
-                other = f << left_shift;
-                m
-            }).rev().collect::<Vec<u64>>()
+    fn mul(self, rhs: Self) -> Number {
+        let se = self.clone();
+        let s2 = format!("{:x}", rhs);
+        let mut res = Number::zero();
+        for (i, ii) in from_hex_to_bin(s2.as_str()).chars().rev().enumerate() {
+            if ii == '1' {
+                let nowww = se.clone() << i as u32;
+                res = &res + &nowww;
+            }
+        }
+        res
+    }
+}
+
+impl Clone for Number {
+    fn clone(&self) -> Self {
+        Number{ v: self.v.clone() }
+    }
+}
+
+impl Shl<u32> for Number {
+    type Output = Number;
+
+    fn shl(self, rhs: u32) -> Self::Output {
+        if rhs == 0 { return  self; }
+        let shl_max_63 = |num: Number, rhs :u32| {
+            let mut vec = Vec::with_capacity(num.v.len());
+            let right_shift = 64 - rhs;
+            let mut other: u64 = 0;
+            for i in num.v {
+                vec.push((i.wrapping_shl(rhs)) | other);
+                other = i.wrapping_shr( right_shift);
+            }
+            vec.push(other);
+            Number { v: vec }
+        };
+        if rhs > 63 {
+            let mut save = self;
+            for _ in 0..rhs / 63 {
+                save = shl_max_63(save, 63u32);
+            }
+            save = shl_max_63(save, rhs % 63);
+            save
+        } else {
+            shl_max_63(self, rhs)
         }
     }
+}
+
+impl Shr<u32> for Number {
+    type Output = Number;
+
+    fn shr(self, rhs: u32) -> Self::Output {
+        if rhs == 0 { return  self; }
+        let mut f = format!("{:#x}", self);
+        f = from_hex_to_bin(f.trim_start_matches("0"));
+        f = f.chars().take(f.len()-(rhs as usize)).collect::<String>();
+        let mut b = String::from("0b");
+        b.push_str(f.as_str());
+        Number::get_from_str(b.as_str()).unwrap()
+    }
+}
+
+pub fn from_hex_to_bin(s: &str) -> String {
+    let mut sss = String::new();
+    for c in s.to_uppercase().chars() {
+        let ddd = match c {
+            '0' => "0000",
+            '1' => "0001",
+            '2' => "0010",
+            '3' => "0011",
+            '4' => "0100",
+            '5' => "0101",
+            '6' => "0110",
+            '7' => "0111",
+            '8' => "1000",
+            '9' => "1001",
+            'A' => "1010",
+            'B' => "1011",
+            'C' => "1100",
+            'D' => "1101",
+            'E' => "1110",
+            'F' => "1111",
+            _ => "",
+        };
+        sss.push_str(ddd);
+    }
+    sss
 }
 
 impl Number {
     pub fn get_from_str(s: &str) -> Result<Number, NumberError> {
         let (prefix, num) = s.trim().split_at(2);
-        let mut vec = Vec::new();
+        let mut vec;
+        if num.is_empty() {
+            return Ok(Number::zero());
+        }
         match prefix {
             "0b" => {
-                if num.is_empty() { return Ok(Number::zero()); }
-                let chunks = num.chars().chunks(64);
+                vec = Vec::new();
+                let chunks = num.chars().rev().chunks(64);
                 for i in &chunks {
-                    let m = i.collect::<String>();
+                    let m = i.collect::<String>().chars().rev().collect::<String>();
                     vec.push(u64::from_str_radix(m.as_str(), 2)?);
                 }
                 Ok(Number { v: vec })
-            }
+            },
             "0x" => {
-                if num.is_empty() { return Ok(Number::zero()); }
-                let chunks = num.chars().chunks(16);
+                vec = Vec::new();
+                let chunks = num.chars().rev().chunks(16);
                 for i in &chunks {
-                    let m = i.collect::<String>();
-                    println!("{m}" );
+                    let m = i.collect::<String>().chars().rev().collect::<String>();
                     vec.push(u64::from_str_radix(m.as_str(), 16)?);
                 }
                 Ok(Number { v: vec })
@@ -193,14 +271,13 @@ impl Number {
     }
 
     pub fn zero() -> Number {
-        let mut v = Vec::new();
-        v.push(0u64);
-        Number { v }
+        Number { v: vec![0u64] }
     }
 
     pub fn one() -> Number {
+        // maybe not
         let mut v = Vec::new();
-        v.push(1u64);
+        v.push(1u64.reverse_bits());
         Number { v }
     }
 }
@@ -208,8 +285,5 @@ impl Number {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
