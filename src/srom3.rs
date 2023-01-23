5fde901e26af7ec25d2268414fb11d455186ef06
DKotov <dkotov292@gmail.com>
2023-01-23 23:17:24 +0200

    bruh :/

--- src/srom3.rs
+++ src/srom3.rs
@@ -1,8 +1,10 @@
-
 use std::cmp::Ordering;
 use std::cmp::Ordering::{Equal, Greater};
-use std::mem;
-use crate::{from_arr_to_string, from_hex_string_to_arr};
+use std::collections::HashSet;
+use Ordering::Less;
+
+
+const GEN: [usize; 5] = [233, 9, 4, 1, 0];
 
 pub fn add_right_zero(a: &str, i: usize) -> String {
     let mut a = String::from(a);
@@ -11,6 +13,8 @@ pub fn add_right_zero(a: &str, i: usize) -> String {
 }
 
 fn compare(a: &str, b: &str) -> Ordering {
+    let a = a.trim_start_matches('0');
+    let b = b.trim_start_matches('0');
     for i in 0..a.len().max(b.len()) {
         let aa = a.chars().nth(i);
         let bb = b.chars().nth(i);
@@ -18,110 +22,148 @@ fn compare(a: &str, b: &str) -> Ordering {
             return Greater;
         }
         if bb.is_some() && aa.is_none() {
-            return Ordering::Less;
+            return Less;
         }
-        if aa.is_some() && bb.is_some() && aa.unwrap() == '1' && bb.unwrap() == '0' {
-            return Ordering::Greater;
+        if aa.is_some()
+            && bb.is_some()
+            && aa.unwrap() == '1'
+            && bb.unwrap() == '0'
+            && a[i..].len() >= b[i..].len()
+        {
+            return Greater;
         }
-        if aa.is_some() && bb.is_some() && bb.unwrap() == '1' && aa.unwrap() == '0' {
-            return Ordering::Less;
+        if aa.is_some()
+            && bb.is_some()
+            && bb.unwrap() == '1'
+            && aa.unwrap() == '0'
+            && a[i..].len() <= b[i..].len()
+        {
+            return Less;
         }
     }
     Equal
 }
 
-pub fn mul(mut a: &str, mut b: &str, c: &str) -> String {
-    let mut a = a.trim_start_matches("0").to_string();
-    let mut b = b.trim_start_matches("0").to_string();
-    let mut res = String::from("0");
-    for i in 0..b.len() - 1 {
-        match char_to_i(b.chars().nth(i).unwrap_or('0')) {
-            0 => {
-                if is_zero(b.as_str()) {
-                    let mut s = String::new();
-                    (0..b.len() + i).for_each(|_| s.push('0'));
-                    b = s
-                }
+//          000101      1101010
+pub fn mul(mut a: &str, mut b: &str) -> String {
+    let mut res = HashSet::new();
+    let a = convert_to_vec(a);
+    let b = convert_to_vec(b);
+    for x in a.iter() {
+        for y in b.iter() {
+            let reees = *x + *y;
+            if !res.remove(&reees) {
+                res.insert(reees);
             }
+        }
+    }
+    let mut res = res.into_iter().collect::<Vec<usize>>();
+    let mut temp_res = convert_to_string(res.clone());
+    res.sort();
+    res.reverse();
+    println!("vector of stepeney: {res:?}");
+    temp_res = modd(temp_res.as_str());
+
+    temp_res
+}
+
+pub fn modd(a: &str) -> String {
+    println!(
+        "{:?}\ngot: {} \ngen: {}\n",
+        compare(a, convert_to_string(GEN.to_vec()).as_str()),
+        a,
+        convert_to_string(GEN.to_vec()).as_str()
+    );
+    match compare(a, convert_to_string(GEN.to_vec()).as_str()) {
+        Greater => {
+            let mut temp_res = a.to_string();
+            let mut conv = convert_to_vec(a);
+            conv.sort();
+            conv.reverse();
+            let (greater, b) = conv.iter().next().unwrap().overflowing_sub(233);
+            if b {
+                return temp_res;
+            }
+            let mut temp = Vec::new();
+            for i in GEN {
+                temp.push(greater + i);
+            }
+            // println!("temp: {temp:?}");
+            temp_res = add(temp_res.as_str(), convert_to_string(temp).as_str());
+            // println!("temp_res: {}", temp_res);
+            modd(temp_res.as_str())
+        }
+        Less => {
+            return a.to_string();
+        }
+        Equal => {
+            return "0".to_string();
+        }
+    }
+}
+
+pub fn convert_to_vec(a: &str) -> Vec<usize> {
+    let mut res = Vec::new();
+    let a = a.chars().rev().collect::<String>();
+    for i in (0..a.len()).rev() {
+        let a: char = a.to_string().chars().nth(i).unwrap();
+        match char_to_i(a) {
             1 => {
-                res = add(res.as_str(),
-                          add_right_zero(a.as_str(), b.len() - i - 1).as_str())
+                res.push(i as usize);
             }
             _ => {}
         }
     }
-    println!("{res}");
-    modd(res.as_str(), c)
-// res
-}
-
-pub fn modd(a: &str, c: &str) -> String {
-    let mut res = a.to_string();
-    if compare(a, c) == Ordering::Less {
-        return a.to_string();
-    }
-    while compare(res.as_str(), c) == Greater || compare(res.as_str(), c) == Equal {
-        let d = add_right_zero(c, res.len() - c.len());
-        res = sub(res.as_str(), d.as_str());
-        res = res.trim_start_matches("0").to_string();
-    }
     res
 }
 
-// pub fn modd(a: &str) -> String {
-//     if a.len() == 233 { return a.to_string(); }
-//     // let mut modd = String::from("100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000100011");
-//     let mut modd = String::from("1");
-//     modd = add_right_zero(modd.as_str(), 233);
-//     let mut res = a.to_string();
-//     while res.len() > 233 {
-//         while let Some(stripped) = res.strip_prefix("0") {
-//             res = String::from(stripped);
-//             if res.len() == 233 {
-//                 return res;
-//             }
-//         }
-//         let mut reserve = add_right_zero(modd.as_str(), res.len() - modd.len() - 1);
-//         res = add(res.chars().as_str(), reserve.as_str());
-//
-//         println!("reverve: {reserve} \nres: {res} \n");
-//     }
-//     res
-// }
-
-pub fn sub(a: &str, b: &str) -> String {
-    let a = a.chars().rev().collect::<String>();
-    let b = b.chars().rev().collect::<String>();
-    let mut res = String::new();
-    for i in 0..b.len() {
-        if a.chars().nth(i).unwrap_or('0') == '1' &&
-            b.chars().nth(i).unwrap_or('0') == '0' ||
-            (a.chars().nth(i).unwrap_or('0') == '0' &&
-                b.chars().nth(i).unwrap_or('0') == '1' && i != b.len() - 1) {
-            res.push('1');
+pub fn convert_to_string(a: Vec<usize>) -> String {
+    let mut s = a.clone();
+    s.sort();
+    let first: usize = *s.iter().rev().next().unwrap();
+    let mut ss = String::new();
+    for i in (0..=first).rev() {
+        if a.contains(&i) {
+            ss.push('1');
         } else {
-            res.push('0');
+            ss.push('0');
         }
     }
-    res.push_str(&a.as_str()[b.len()..]);
-    res.chars().rev().collect::<String>()
+    ss.chars().collect::<String>()
 }
 
-pub fn add(a: &str, b: &str) -> String {
-    let mut a = a.chars().rev().collect::<String>();
-    let mut b = b.chars().rev().collect::<String>();
-    let mut res = String::new();
-    if a.len() < b.len() {
-        mem::swap(&mut a, &mut b);
-    }
-    for i in 0..b.len() {
-        let rrres = char_to_i(a.chars().nth(i).unwrap_or('0'))
-            ^ char_to_i(b.chars().nth(i).unwrap_or('0'));
-        res.push_str(rrres.to_string().chars().as_str());
-    }
-    res.push_str(&a[b.len()..]);
+// pub fn modd(a: &str) -> {}
 
-    res.chars().rev().collect::<String>()
+pub fn add(a1: &str, b1: &str) -> String {
+    // let mut a: String = a.chars().rev().collect::<String>();
+    // let mut b: String = b.chars().rev().collect::<String>();
+    // let mut res = String::new();
+    //
+    // for i in 0..(b.len().max(a.len())) {
+    //     let rrres = char_to_i(a.chars().nth(i).unwrap_or('0'))
+    //         ^ char_to_i(b.chars().nth(i).unwrap_or('0'));
+    //     res.push_str(rrres.to_string().chars().as_str());
+    // }
+    // let res =  res.chars().rev().collect::<String>().trim_start_matches('0').to_string();
+    // println!("a:{a} \n+\nb:{b}\nc:{res}");
+    // res
+    let a = convert_to_vec(a1);
+    println!("{a:?}");
+    let b = convert_to_vec(b1);
+    println!("{b:?}");
+    let mut res = HashSet::new();
+    a.iter().chain(b.iter()).for_each(|&each| {
+        if !res.remove(&each) {
+            res.insert(each);
+        }
+    });
+    let mut res = res.into_iter().collect::<Vec<usize>>();
+    res.sort();
+    res.reverse();
+    println!("stepenya: {res:?}");
+    let res = convert_to_string(res);
+    println!("a:{a1} \n+\nb:{b1}\nc:{res}");
+    res
 }
 
 pub fn is_zero(a: &str) -> bool {
@@ -132,6 +174,6 @@ pub fn char_to_i(i: char) -> u8 {
     match i {
         '0' => 0,
         '1' => 1,
-        _ => panic!("in char to int function there are problem")
+        _ => panic!("in char to int function there are problem"),
     }
 }
--- src/srom4.rs
+++ src/srom4.rs
@@ -1,64 +1,208 @@
-use std::mem;
+use std::cmp::Ordering;
+use std::cmp::Ordering::{Equal, Greater};
+use std::collections::HashSet;
+use Ordering::Less;
 
-pub fn add(a: &str, b: &str) -> String {
-    let mut a = a;
-    let mut b = b;
-    let mut res = String::new();
+const GEN: [usize; 5] = [233, 9, 4, 1, 0];
+static mut Matrix: Vec<Vec<i16>> = Vec::new();
 
-    for i in 0..233 {
-        let rrres;
-        if a.chars().nth(i).is_none() && b.chars().nth(i).is_none() {
-            rrres = '0';
-        } else {
-            rrres = char::from_u32(a.chars().nth(i).unwrap_or('0').to_digit(2).unwrap_or(0)
-                ^ b.chars().nth(i).unwrap_or('0').to_digit(2).unwrap_or(0) + 48).unwrap_or('0');
-        }
-        res.push(rrres);
-    }
-    res
+pub fn add_right_zero(a: &str, i: usize) -> String {
+    let mut a = String::from(a);
+    (0..i).for_each(|_| a.push('0'));
+    a
 }
 
-pub fn mul(a: &str, b: &str) -> String {
+pub unsafe fn mul(a: &str, b: &str) -> String {
     let mut a = a.to_string();
     let mut b = b.to_string();
-    let mut res = String::new();
-    let mut v: Vec<Vec<i32>> = Vec::new();
-    let p = 2 * 233 + 1;
+    let mut result = Vec::with_capacity(233);
     for i in 0..233 {
-        v.push(Vec::new());
-        for o in 0..233 {
-            if (2i128.pow(i) - 2i128.pow(o)) % p != 1 &&
-                (-2i128.pow(i) - 2i128.pow(o)) % p != 1 &&
-                (2i128.pow(o) - 2i128.pow(i)) % p != 1 &&
-                (2i128.pow(i) + 2i128.pow(o)) % p != 1 {
-                v.get_mut(i as usize).unwrap().push(0);
-            } else {
-                v.get_mut(i as usize).unwrap().push(1);
+        let oh = mul_matrix(
+            &mul_matrix(&bin_str_to_vec(a.as_str()), &Matrix),
+            &transpose(&bin_str_to_vec(b.as_str())),
+        );
+        result.push(oh[0][0]);
+        a = cshl(a.as_str());
+        b = cshl(b.as_str());
+    }
+    result.iter().map(|i| i.to_string()).collect::<String>()
+}
+
+pub fn bin_str_to_vec(a: &str) -> Vec<Vec<i16>> {
+    let mut result = Vec::new();
+    (0..233).for_each(|_| {
+        let mut vec = Vec::new();
+        (0..233).for_each(|_| vec.push(0i16));
+        result.push(vec)
+    });
+    for (i, u) in a.chars().map(|char| char_to_i(char)).enumerate() {
+        match u {
+            1 => {
+                result[0][i] = 1;
             }
+            _ => {}
         }
     }
-    for _ in 0..233 {
-        let mut new_vec = Vec::new();
-        for i in 0..233 {
-            let mut m = 0;
-            for j in 0..233 {
-                m = (m + a.chars().nth(j).unwrap().to_digit(2).unwrap() as i32 *
-                    v.get(j).unwrap().get(i).unwrap() ) % 2;
-            }
-            new_vec.push(m);
+    result
+}
+
+pub fn transpose(a: &Vec<Vec<i16>>) -> Vec<Vec<i16>> {
+    let mut result = Vec::with_capacity(a.first().unwrap().len());
+    (0..a.first().unwrap().len()).for_each(|_| {
+        let mut vec = Vec::new();
+        (0..a.len()).for_each(|_| vec.push(0i16));
+        result.push(vec)
+    });
+    for i in 0..a.len() {
+        for j in 0..a.first().unwrap().len() {
+            result[j][i] = a[i][j];
         }
-        let mut m = 0;
-        (0..233).for_each(|ii| m = (m +
-            new_vec.get(ii).unwrap() * b.chars().nth(ii).unwrap().to_digit(2).unwrap() as i32 ) % 2);
-        res.push_str(m.to_string().as_str());
-        a = shift(a.as_str());
-        b = shift(b.as_str());
+    }
+    result
+}
+
+pub fn mul_matrix(a: &Vec<Vec<i16>>, b: &Vec<Vec<i16>>) -> Vec<Vec<i16>> {
+    let a_col_len = a.first().unwrap().len();
+    let b_row_len = b.len();
+    if a_col_len != b_row_len {
+        panic!("we can't multiply matrix with this lens")
+    }
+    let a_row_len = a.len();
+    let b_col_len = b.first().unwrap().len();
+    let mut res = Vec::new();
+    (0..a_row_len).for_each(|_| {
+        let mut vec = Vec::with_capacity(b_col_len);
+        (0..b_col_len).for_each(|_| vec.push(0i16));
+        res.push(vec)
+    });
+    for i in 0..a_row_len {
+        for j in 0..b_col_len {
+            for k in 0..a_col_len {
+                res[i][j] += a[i][k] * b[k][j];
+                res[i][j] %= 2;
+            }
+        }
     }
     res
 }
 
-pub fn shift(s: &str) -> String {
-    let mut ss = String::from(&s[1..]);
-    ss.push(s.chars().nth(0).unwrap());
-    ss
+pub fn cshl(a: &str) -> String {
+    let mut res = String::from(&a[..a.len()-1]);
+    res.push_str(&a[a.len()-1..]);
+    res
+}
+
+pub unsafe fn matrix_find() {
+    let p: i128 = 2 * 233 + 1;
+    // let exist_onb = {
+    //     let is_prime = {
+    //         let mut pr = true;
+    //         for i in 2..p {
+    //             if (p % i) == 0 {
+    //                 pr = false;
+    //                 break;
+    //             }
+    //         }
+    //         pr
+    //     };
+    //     if is_prime {
+    //         if 2i128.pow(2 * 233) % p == 1 {
+    //             true
+    //         } else {
+    //             p % 4 == 3 && 2i128.pow(233) % p == 1
+    //         }
+    //     } else {
+    //         false
+    //     }
+    // };
+    // if !exist_onb {
+    //     panic!("no...");
+    // }
+    Matrix = Vec::new();
+    (0..233).for_each(|_| {
+        let mut vec = Vec::with_capacity(233);
+        (0..233).for_each(|_| vec.push(0i16));
+        Matrix.push(vec)
+    });
+
+    for i in 0..233 {
+        for j in 0..233 {
+            let c1 = 2f64.powf(i as f64) + 2f64.powf(j as f64);
+            let mut c2 = 2f64.powf(i as f64) - 2f64.powf(j as f64);
+            let mut c3 = -2f64.powf(i as f64) + 2f64.powf(j as f64);
+            let c4 = (-22f64.powf(i as f64) - 2f64.powf(j as f64)) % p as f64 + p as f64;
+            if c2 < 0f64 {
+                c2 = c2 % p as f64 + p as f64;
+            }
+            if c3 < 0f64 {
+                c3 = c3 % p as f64 + p as f64;
+            }
+
+            if c1 % p as f64 == 1 as f64
+                || c2 % p as f64 == 1 as f64
+                || c3 % p as f64== 1 as f64
+                || c4 % p as f64 == 1 as f64
+            {
+                Matrix[i as usize][j as usize] = 1;
+            }
+            // else {
+            //     Matrix[i as usize][j as usize] = 0;
+            // }
+        }
+    }
+}
+
+pub fn convert_to_vec(a: &str) -> Vec<usize> {
+
+    let mut res = Vec::new();
+    let a = a.chars().rev().collect::<String>();
+    for i in (0..a.len()).rev() {
+        let a: char = a.to_string().chars().nth(i).unwrap();
+        match char_to_i(a) {
+            1 => {
+                res.push(i as usize);
+            }
+            _ => {}
+        }
+    }
+    res
+}
+
+pub fn convert_to_string(a: Vec<usize>) -> String {
+    let mut s = a.clone();
+    s.sort();
+    let first: usize = *s.iter().rev().next().unwrap();
+    let mut ss = String::new();
+    for i in (0..=first).rev()
+    {
+        if a.contains(&i) {
+            ss.push('1');
+        } else {
+            ss.push('0');
+        }
+    }
+    ss.chars().collect::<String>()
+}
+
+pub fn add(a1: &str, b1: &str) -> String {
+    let a = convert_to_vec(a1);
+    let b = convert_to_vec(b1);
+    let mut res = HashSet::new();
+    a.iter().chain(b.iter()).for_each(|&each| {
+        if !res.remove(&each) {
+            res.insert(each);
+        }
+    });
+    let mut res = res.into_iter().collect::<Vec<usize>>();
+    res.sort();
+    res.reverse();
+    convert_to_string(res)
+}
+
+pub fn char_to_i(i: char) -> u8 {
+    match i {
+        '0' => 0,
+        '1' => 1,
+        _ => panic!("in char to int function there are problem"),
+    }
 }
